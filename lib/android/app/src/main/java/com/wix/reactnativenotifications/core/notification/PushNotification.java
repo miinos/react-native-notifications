aede448ad61fbf5d58f7386f76a0ad40bc75f92a
Amine SOUAIAIA <amine@Amines-MacBook-Pro.local>
2020-03-10 00:59:10 +0100

    add bitmap import

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -6,6 +6,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
